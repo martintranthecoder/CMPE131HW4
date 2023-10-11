bfe7af9544143968f4c1b043b4a6a81d875c6ee3
Martin Tran <106730912+martintranthecoder@users.noreply.github.com>
2023-10-11 10:22:53 -0700

    Update ladder.py
    update error

--- ladder.py
+++ ladder.py
@@ -2,4 +2,4 @@
 def my_steps(self, n: int) -> int:
     if n == 0 or n == 1:
         return 1
-    return self.my_steps(n-1) + self.climbStairs(n-2)
+    return self.my_steps(n-1) + self.my_steps(n-2)
