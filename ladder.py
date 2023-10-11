934e0f9c66b794d5ebf2a4a58d9040d2e980baeb
Martin Tran <106730912+martintranthecoder@users.noreply.github.com>
2023-10-11 10:44:36 -0700

    Update ladder.py
    update new exception

--- ladder.py
+++ ladder.py
@@ -1,6 +1,10 @@
 
 def my_steps(n: int):
-    if n == 0 or n == 1:
+    if n < 1 or n > 25
+        raise ValueError("Interger n should be between 1 and 25")
+
+    
+    if n == 1:
         return 1
     elif n == 2:
         return 2
