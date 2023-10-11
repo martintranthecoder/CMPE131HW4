19385a130b5c7e7e32a3602d7d3518ec02bd8330
Martin Tran <106730912+martintranthecoder@users.noreply.github.com>
2023-10-11 10:41:44 -0700

    Update ladder.py
    new update

--- ladder.py
+++ ladder.py
@@ -1,8 +1,8 @@
 
-def my_steps(self, n: int):
+def my_steps(n: int):
     if n == 0 or n == 1:
         return 1
     elif n == 2:
         return 2
         
-    return self.my_steps(n-1) + self.my_steps(n-2)
+    return my_steps(n-1) + my_steps(n-2)
